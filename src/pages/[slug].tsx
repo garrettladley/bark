68509cdcbc6a6333b4e7bb3bdf76b4577d3414b1
garrettladley <ladley.g@northeastern.edu>
2023-04-23 12:05:57 -0400

    fixed lint failure

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,11 +1,11 @@
 import type { GetStaticProps, NextPage } from "next";
-import Head from "next/head";
-import { api } from "~/utils/api";
-import { PageLayout } from "~/components/layout";
-import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
-import { PostView } from "~/components/postview";
-import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import Head from 'next/head';
+import Image from 'next/image';
+import { PageLayout } from '~/components/layout';
+import { LoadingPage } from '~/components/loading';
+import { PostView } from '~/components/postview';
+import { generateSSGHelper } from '~/server/helpers/ssgHelper';
+import { api } from '~/utils/api';
 
 const ProfileFeed = (props: { userId: string }) => {
   const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
@@ -30,18 +30,17 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
     username,
   });
   if (!data) return <div>404</div>;
+
   return (
     <>
       <Head>
-        <title>{data.username ?? data.externalUsername}</title>
+        <title>{data.username}</title>
       </Head>
       <PageLayout>
         <div className="relative h-36 bg-slate-600">
           <Image
             src={data.profileImageUrl}
-            alt={`${
-              data.username ?? data.externalUsername ?? "unknown"
-            }'s profile pic`}
+            alt={`${data.username ?? ""}'s profile pic`}
             width={128}
             height={128}
             className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
@@ -49,7 +48,7 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         </div>
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${
-          data.username ?? data.externalUsername ?? "unknown"
+          data.username ?? ""
         }`}</div>
         <div className="w-full border-b border-slate-400" />
         <ProfileFeed userId={data.id} />
